161a2eaf7b0e3622114a489fbb666ba22eb1aa3d
SelimC06 <scoskunuzer@gmail.com>
2023-04-25 09:31:53 -0500

    Initial Commit

---
+++ Mynance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mynance
+//
+//  Created by Coskunuzer, Selim on 4/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
